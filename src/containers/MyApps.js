01aacb51d6f2a20b30802e38322acc24b9850980
Julian Miqueo <kuliantxo@gmail.com>
2016-07-19 10:27:56 -0700

    ajax and json

--- src/containers/MyApps.js
+++ src/containers/MyApps.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import AppCardBoxComp from '../components/Cards.js';
 
-var appCards = {
-  header: 'My Apps',
-  cards: [
-    {id: 1, appName: "Pete Hunt", vendorName: "This is one comment", tagline: "This is the tagline is one comment"},
-    {id: 2, appName: "Jordan Walke", vendorName: "This is another comment", tagline: "This is the tagline is one comment"},
-    {id: 3, appName: "Pete Hunt Pepe", vendorName: "This is vendor name", tagline: "This is the tagline is one comment"},
-    {id: 4, appName: "Jordan Walke Maria", vendorName: "This is vendorname", tagline: "This is the tagline is one comment 2"},
-    {id: 5, appName: "Jordan Walke", vendorName: "This is another comment", tagline: "This is the tagline is one comment"},
-    {id: 6, appName: "Jordan Walke Maria", vendorName: "This is vendorname", tagline: "This is the tagline is one comment 2"},
-    {id: 7, appName: "Pete Hunt", vendorName: "This is one comment", tagline: "This is the tagline is one comment"},
-    {id: 8, appName: "Pete Hunt Pepe", vendorName: "This is vendor name", tagline: "This is the tagline is one comment"}
-  ]
-};
-
 let Home = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: "my.json",
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render() {
     return(
-      <AppCardBoxComp data={ appCards } />
+      <AppCardBoxComp data={ this.state.data } />
     );
   }
 });
